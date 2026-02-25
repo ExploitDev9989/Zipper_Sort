c448b038886e91c978e400abdd17b45813fedfa5
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-02-25 13:37:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Zipper_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Emter another word: ");
+            string word2 = Console.ReadLine();
+            string final = "";
+            string longWord = "";
+            string shortWord = "";
+            if (word2.Length > word1.Length)
+            {
+                longWord = word2;
+                shortWord = word1;
+            }
+            else
+            {
+                longWord = word1;
+                shortWord = word2;
+            }
+            for (int i = 0; i < longWord.Length; i++)
+            {
+                if (i < word1.Length)
+                {
+                    final += word1[i];
+                    
+                }
+                if (i < word2.Length) 
+                {
+                    final += word2[i];
+                }
+                
+            }
+            Console.WriteLine(final);
+
+
+
+
+
+
+
+
+        }
+    }
+}
